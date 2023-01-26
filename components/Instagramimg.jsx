482b889e5aaa1f1a4cda6de16c1f40539d242b07
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-01-26 11:32:31 +0100

    add img

---
+++ components/Instagramimg.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Instagramimg = () => {
+  return (
+    <div>Instagramimg</div>
+  )
+}
+
+export default Instagramimg
--- components/SliderData.js
+++ components/SliderData.js
@@ -15,8 +15,4 @@ export const SliderData = [
       image:
         'https://images.unsplash.com/photo-1484591974057-265bb767ef71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
     },
-    {
-      image:
-        'https://images.unsplash.com/photo-1545917992-dea2d782ef46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2489&q=80',
-    },
   ];
